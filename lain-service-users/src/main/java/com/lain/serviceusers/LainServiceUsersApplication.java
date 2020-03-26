712e1d4f75fe4d7afa1e7900f55b816f23d69bd0
lainx1 <ehecattl@hotmail.com>
2020-03-25 21:11:04 -0600

    add eureka, zuul server

--- lain-service-users/src/main/java/com/lain/serviceusers/LainServiceUsersApplication.java
+++ lain-service-users/src/main/java/com/lain/serviceusers/LainServiceUsersApplication.java
@@ -2,8 +2,10 @@ package com.lain.serviceusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaAuditing
 public class LainServiceUsersApplication {
